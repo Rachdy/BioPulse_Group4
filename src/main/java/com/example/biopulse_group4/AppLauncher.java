cffe10fa2b5648c69fc6be30e1d2de417481b966
Reda3107 <makroumreda999@gmail.com>
2026-05-11 15:52:28 +0200

    Fixed s1 duplicate and added s2 s3

--- src/main/java/com/example/biopulse_group4/AppLauncher.java
+++ src/main/java/com/example/biopulse_group4/AppLauncher.java
@@ -6,16 +6,23 @@ import java.time.LocalDate;
 public class AppLauncher {
     public static void main(String[] args) {
 
-        // Testing the Model Layer
+        // 1. Creating animals
         Animal lion = new Mammal("M001", "African Lion", "Mammalia", "Savanna", "Vulnerable", true, "Golden");
         Animal eagle = new Bird("B001", "Golden Eagle", "Aves", "Mountains", "Least Concern", 2.2, true);
         Animal cobra = new Reptile("R001", "King Cobra", "Reptilia", "Forest", "Vulnerable", true, "Smooth");
 
+        // 2. Creating sightings
         Sighting s1 = new Sighting("S001", lion, LocalDate.now(), "Kruger Park", 3, "Healthy pride.");
+        Sighting s2 = new Sighting("S002", eagle, LocalDate.now(), "Atlas", 1, "High altitude.");
+        Sighting s3 = new Sighting("S003", cobra, LocalDate.now(), "Vietnam", 1, "Near river.");
 
+        // 3. Displaying details
         System.out.println("========== BIOPULSE TEST ==========");
         s1.displaySightingDetails();
-        cobra.displayInfo();
+        System.out.println();
+        s2.displaySightingDetails();
+        System.out.println();
+        s3.displaySightingDetails();
         System.out.println("===================================");
     }
 }
