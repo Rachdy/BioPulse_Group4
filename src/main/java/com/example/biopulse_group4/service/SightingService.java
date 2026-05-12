f762e507aa892fae8379b53058db666c8e9872aa
zshgpg9tmv-sketch <zshgpg9tmv@privaterelay.appleid.com>
2026-05-12 10:44:59 +0200

    added sighting and validation service

--- src/main/java/com/example/biopulse_group4/service/SightingService.java
+++ src/main/java/com/example/biopulse_group4/service/SightingService.java
@@ -1,4 +1,33 @@
 package com.example.biopulse_group4.service;
+import com.example.biopulse_group4.Model.Sighting;
+//
+import java.util.ArrayList;
 
 public class SightingService {
+    //List of Sightings
+    private ArrayList<Sighting> sightings = new ArrayList<>();
+
+    public boolean addSighting(Sighting sighting) {
+        return true;
+    }
+
+    public ArrayList<Sighting> getAllSightings() {
+        return sightings;
+    }
+
+    public ArrayList<Sighting> searchSightings(String location) {
+        return sightings;
+    }
+
+    public ArrayList<Sighting> filterSightings() {
+        return sightings;
+    }
+
+    public boolean updateSighting(Sighting sighting) {
+        return true;
+    }
+
+    public boolean removeSighting(String sightingID) {
+        return true;
+    }
 }
--- src/main/java/com/example/biopulse_group4/service/ValidationService.java
+++ src/main/java/com/example/biopulse_group4/service/ValidationService.java
@@ -1,4 +1,16 @@
 package com.example.biopulse_group4.service;
 
-public class ValidationService {
+import java.time.LocalDate;
+
+public class ValidationService {public boolean validateDate(LocalDate date) {
+    return true;
+}
+
+    public boolean validateCount(int count) {
+        return true;
+    }
+
+    public boolean validateLocation(String location) {
+        return true;
+    }
 }
