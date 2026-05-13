2d38e7d3ca83989e876b7416ac3aab17d6dc4371
zshgpg9tmv-sketch <zshgpg9tmv@privaterelay.appleid.com>
2026-05-13 12:07:59 +0200

    update sightings

--- src/main/java/com/example/biopulse_group4/service/ValidationService.java
+++ src/main/java/com/example/biopulse_group4/service/ValidationService.java
@@ -5,11 +5,17 @@ import com.example.biopulse_group4.Model.Sighting;
 
 public class ValidationService {
     public boolean validateDate(LocalDate date) {
+        if (date == null){
+            return false;
+        }
+        if (date.isAfter(LocalDate.now())){
+            return false;
+        }
     return true;
 }
 
     public boolean validateCount(int count) {
-        return true;
+        return count>0;
     }
 
     public boolean validateLocation(String location) {
